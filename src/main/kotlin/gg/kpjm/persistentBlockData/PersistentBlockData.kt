91a098ea0570138dab11a46db2191a3cd6b63a40
Kolja <kolja.menzel@outlook.com>
2026-01-30 16:23:14 +0100

    Initial Commit

---
+++ src/main/kotlin/gg/kpjm/persistentBlockData/PersistentBlockData.kt
@@ -0,0 +1,14 @@
+package gg.kpjm.persistentBlockData
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class PersistentBlockData : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
